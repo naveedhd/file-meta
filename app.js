75d16bfbea2de87826e24c1088307a17bd631d15
Naveed Usmani <naveedhd@gmail.com>
2016-04-25 10:37:36 +0900

    Reaps uploaded files

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 var express = require('express');
 var multer  = require ('multer');
 var upload  = multer({ dest: 'uploads/'});
-var autoReap  = require('multer-autoreap');
+var fs      = require('fs');
 
 var app     = express();
 var port    = process.env.PORT || 3000;
 
 app.use(multer({ dest: 'uploads/'}).single('0'));
-app.use(autoReap);
-
-//var type    = upload.single('0');
 
 app.post('/api/fileanalyse/', function(req, res) {
-  console.log(req.file);
   res.send({
     fileSize: req.file.size
+  });
+  fs.stat(req.file.path, function(err, stats) {
+    if (!err && stats.isFile()) {
+      fs.unlink(req.file.path);
+      console.log('removed %s', req.file.path);
+    }
   });
   res.end();
 });
