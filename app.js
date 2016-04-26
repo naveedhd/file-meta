15ae332f1f7cb172d41b6487dc5bff900694d443
Naveed Usmani <naveedhd@gmail.com>
2016-04-26 17:37:45 +0900

    Using multer-autoreap for deleting files

--- app.js
+++ app.js
@@ -2,22 +2,18 @@ var express = require('express');
 var multer  = require ('multer');
 var upload  = multer({ dest: 'uploads/'});
 var fs      = require('fs');
+var autoReap = require('multer-autoreap');
 
 var app     = express();
 var port    = process.env.PORT || 3000;
 
 app.use(multer({ dest: 'uploads/'}).single('0'));
+app.use(autoReap);
 
 app.post('/api/fileanalyse/', function(req, res) {
   res.send({
     fileSize: req.file.size
-  });
-  fs.stat(req.file.path, function(err, stats) {
-    if (!err && stats.isFile()) {
-      fs.unlink(req.file.path);
-      console.log('removed %s', req.file.path);
-    }
-  });
+  });  
   res.end();
 });
 
